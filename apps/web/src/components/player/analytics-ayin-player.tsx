2ad90f8f2d469e38f68cfb9a879b7dfab83c65c0
eltx1 <info.eltx@gmail.com>
2026-08-30 03:22:25 +0300

    feat: track content impressions

--- apps/web/src/components/player/analytics-ayin-player.tsx
+++ apps/web/src/components/player/analytics-ayin-player.tsx
@@ -1,13 +1,16 @@
 "use client";
 
-import { useMemo } from "react";
+import { useEffect, useMemo } from "react";
 
-import { createPlayerAnalytics } from "@/lib/analytics";
+import { createPlayerAnalytics, trackAnalyticsEvent } from "@/lib/analytics";
 
 import { AdEnabledAyinPlayer } from "./ad-enabled-ayin-player";
 import type { AyinPlayerProps } from "./ayin-player";
 
 export function AnalyticsAyinPlayer(props: AyinPlayerProps) {
   const analytics = useMemo(() => createPlayerAnalytics(props.profileId), [props.profileId]);
+  useEffect(() => {
+    trackAnalyticsEvent("CONTENT_IMPRESSION", { videoId: props.videoId });
+  }, [props.videoId]);
   return <AdEnabledAyinPlayer {...props} analytics={analytics} />;
 }
