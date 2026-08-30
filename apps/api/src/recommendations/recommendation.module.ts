f2af20579163862fe23e6b7ad4ce77e0d335ffb4
eltx1 <info.eltx@gmail.com>
2026-08-30 20:35:01 +0300

    Wire auth into recommendation module

--- apps/api/src/recommendations/recommendation.module.ts
+++ apps/api/src/recommendations/recommendation.module.ts
@@ -1,12 +1,13 @@
 import { Module } from "@nestjs/common";
 
+import { AuthModule } from "../auth/auth.module.js";
 import { DatabaseModule } from "../database/database.module.js";
 import { PlatformConfigModule } from "../platform-config/platform-config.module.js";
 import { RecommendationController } from "./recommendation.controller.js";
 import { RecommendationService } from "./recommendation.service.js";
 
 @Module({
-  imports: [DatabaseModule, PlatformConfigModule],
+  imports: [AuthModule, DatabaseModule, PlatformConfigModule],
   controllers: [RecommendationController],
   providers: [RecommendationService],
   exports: [RecommendationService],
