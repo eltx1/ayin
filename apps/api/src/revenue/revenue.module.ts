550bb0e89e5a9536516755f412543c899c693a26
eltx1 <info.eltx@gmail.com>
2026-08-31 03:18:56 +0300

    feat(revenue): wire manual payout provider adapter

--- apps/api/src/revenue/revenue.module.ts
+++ apps/api/src/revenue/revenue.module.ts
@@ -5,13 +5,28 @@ import { AuthModule } from "../auth/auth.module.js";
 import { DatabaseModule } from "../database/database.module.js";
 import { CreatorFinanceRepository } from "./creator-finance.repository.js";
 import { CreatorFinanceService } from "./creator-finance.service.js";
+import {
+  ManualPayoutProviderAdapter,
+  PAYOUT_PROVIDER_ADAPTER,
+  type PayoutProviderAdapter,
+} from "./payout-provider.adapter.js";
 import { AdminRevenueController, CreatorRevenueController } from "./revenue.controller.js";
 import { RevenueService } from "./revenue.service.js";
 
 @Module({
   imports: [DatabaseModule, AuthModule, AdminModule],
   controllers: [CreatorRevenueController, AdminRevenueController],
-  providers: [RevenueService, CreatorFinanceRepository, CreatorFinanceService],
+  providers: [
+    RevenueService,
+    CreatorFinanceRepository,
+    CreatorFinanceService,
+    ManualPayoutProviderAdapter,
+    {
+      provide: PAYOUT_PROVIDER_ADAPTER,
+      inject: [ManualPayoutProviderAdapter],
+      useFactory: (adapter: ManualPayoutProviderAdapter): PayoutProviderAdapter => adapter,
+    },
+  ],
   exports: [RevenueService, CreatorFinanceService],
 })
 export class RevenueModule {}
