886f99f2fe9d2c5230b742bef6a23ba9c144837f
eltx1 <info.eltx@gmail.com>
2026-08-31 03:12:02 +0300

    feat(revenue): register creator finance service

--- apps/api/src/revenue/revenue.module.ts
+++ apps/api/src/revenue/revenue.module.ts
@@ -4,13 +4,14 @@ import { AdminModule } from "../admin/admin.module.js";
 import { AuthModule } from "../auth/auth.module.js";
 import { DatabaseModule } from "../database/database.module.js";
 import { CreatorFinanceRepository } from "./creator-finance.repository.js";
+import { CreatorFinanceService } from "./creator-finance.service.js";
 import { AdminRevenueController, CreatorRevenueController } from "./revenue.controller.js";
 import { RevenueService } from "./revenue.service.js";
 
 @Module({
   imports: [DatabaseModule, AuthModule, AdminModule],
   controllers: [CreatorRevenueController, AdminRevenueController],
-  providers: [RevenueService, CreatorFinanceRepository],
-  exports: [RevenueService],
+  providers: [RevenueService, CreatorFinanceRepository, CreatorFinanceService],
+  exports: [RevenueService, CreatorFinanceService],
 })
 export class RevenueModule {}
