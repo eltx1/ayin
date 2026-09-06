5cb32646a472ca318b0ccaad40a80a0720b9f1b4
eltx1 <info.eltx@gmail.com>
2026-09-07 02:11:45 +0300

    Add crawler policy and sitemap discovery

---
+++ apps/web/src/app/robots.ts
@@ -0,0 +1,29 @@
+import type { MetadataRoute } from "next";
+
+import { absoluteUrl, AYIN_SITE_URL } from "@/lib/seo";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: [
+          "/admin/",
+          "/studio/",
+          "/account/",
+          "/channel/",
+          "/upload",
+          "/my-ayin",
+          "/search",
+          "/login",
+          "/register",
+          "/forgot-password",
+          "/reset-password",
+        ],
+      },
+    ],
+    host: AYIN_SITE_URL,
+    sitemap: absoluteUrl("/sitemap.xml"),
+  };
+}
