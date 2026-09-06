efb800138651878b32d78c88df6eba601be9aad8
eltx1 <info.eltx@gmail.com>
2026-09-07 02:10:10 +0300

    Register sitemap counts controller

--- apps/api/src/seo/seo.module.ts
+++ apps/api/src/seo/seo.module.ts
@@ -1,10 +1,11 @@
 import { Module } from "@nestjs/common";
 
+import { SeoSitemapCountsController } from "./seo-sitemap-counts.controller.js";
 import { SeoController } from "./seo.controller.js";
 import { SeoService } from "./seo.service.js";
 
 @Module({
-  controllers: [SeoController],
+  controllers: [SeoController, SeoSitemapCountsController],
   providers: [SeoService],
 })
 export class SeoModule {}
