d869e10976306c3e730623a6e9538b2146059fd2
eltx1 <info.eltx@gmail.com>
2026-09-15 02:07:53 +0300

    feat(api): register series sitemap feed

--- apps/api/src/series-catalog/series-catalog.module.ts
+++ apps/api/src/series-catalog/series-catalog.module.ts
@@ -6,10 +6,11 @@ import { VideoPolicyModule } from "../video-policy/video-policy.module.js";
 import { AdminSeriesCatalogController } from "./admin-series-catalog.controller.js";
 import { PublicSeriesCatalogController } from "./series-catalog.controller.js";
 import { SeriesCatalogService } from "./series-catalog.service.js";
+import { SeriesSitemapController } from "./series-sitemap.controller.js";
 
 @Module({
   imports: [AuthModule, AdminModule, VideoPolicyModule],
-  controllers: [PublicSeriesCatalogController, AdminSeriesCatalogController],
+  controllers: [PublicSeriesCatalogController, SeriesSitemapController, AdminSeriesCatalogController],
   providers: [SeriesCatalogService],
   exports: [SeriesCatalogService],
 })
