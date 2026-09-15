ec860894c8ea9d39e927b783192933ed76a49746
eltx1 <info.eltx@gmail.com>
2026-09-16 00:50:55 +0300

    Register regional discovery service

--- apps/api/src/discovery/discovery.module.ts
+++ apps/api/src/discovery/discovery.module.ts
@@ -6,11 +6,12 @@ import { SeriesCatalogModule } from "../series-catalog/series-catalog.module.js"
 import { VideoPolicyModule } from "../video-policy/video-policy.module.js";
 import { DiscoveryController, PublicDiscoveryController } from "./discovery.controller.js";
 import { DiscoveryService, HomeRowConfigService } from "./discovery.service.js";
+import { RegionalDiscoveryService } from "./regional-discovery.service.js";
 
 @Module({
   imports: [AuthModule, DatabaseModule, SeriesCatalogModule, VideoPolicyModule],
   controllers: [PublicDiscoveryController, DiscoveryController],
-  providers: [HomeRowConfigService, DiscoveryService],
-  exports: [HomeRowConfigService, DiscoveryService],
+  providers: [HomeRowConfigService, RegionalDiscoveryService, DiscoveryService],
+  exports: [HomeRowConfigService, RegionalDiscoveryService, DiscoveryService],
 })
 export class DiscoveryModule {}
