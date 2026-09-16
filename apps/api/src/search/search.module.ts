e26b454886b9539f0eb72fcb46f4cc11028bcc70
eltx1 <info.eltx@gmail.com>
2026-09-16 16:15:44 +0300

    Wire language-aware PostgreSQL search

--- apps/api/src/search/search.module.ts
+++ apps/api/src/search/search.module.ts
@@ -5,6 +5,7 @@ import { MovieCatalogModule } from "../movie-catalog/movie-catalog.module.js";
 import { PlatformConfigModule } from "../platform-config/platform-config.module.js";
 import { SeriesCatalogModule } from "../series-catalog/series-catalog.module.js";
 import { VideoPolicyModule } from "../video-policy/video-policy.module.js";
+import { LanguageAwarePostgresSearchService } from "./language-aware-postgres-search.service.js";
 import {
   AYIN_LENS_SEARCH_PROVIDER,
   UnconfiguredAyinLensSearchProvider,
@@ -12,6 +13,7 @@ import {
 import { LensSearchService } from "./lens-search.service.js";
 import { PostgresSearchService } from "./search-postgres.service.js";
 import { SearchController } from "./search.controller.js";
+import { SearchLanguageContextService } from "./search-language-context.service.js";
 import { SearchRateLimiter } from "./search-rate-limiter.js";
 import { SearchService } from "./search.service.js";
 
@@ -26,7 +28,12 @@ import { SearchService } from "./search.service.js";
   controllers: [SearchController],
   providers: [
     SearchService,
-    PostgresSearchService,
+    SearchLanguageContextService,
+    LanguageAwarePostgresSearchService,
+    {
+      provide: PostgresSearchService,
+      useExisting: LanguageAwarePostgresSearchService,
+    },
     LensSearchService,
     SearchRateLimiter,
     UnconfiguredAyinLensSearchProvider,
