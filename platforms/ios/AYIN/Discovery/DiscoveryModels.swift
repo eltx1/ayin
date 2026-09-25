c6eb0c708f0430fa341ff252e3d31d0f172f137b
eltx1 <info.eltx@gmail.com>
2026-09-25 04:56:22 +0300

    task 80: add native auth discovery and deep links

---
+++ platforms/ios/AYIN/Discovery/DiscoveryModels.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+struct DiscoveryHomeResponse: Decodable {
+    let rows: [DiscoveryRow]
+}
+
+struct DiscoveryRow: Decodable, Identifiable {
+    let key: String
+    let title: String
+    let items: [DiscoveryItem]
+    let availability: String?
+
+    var id: String { key }
+}
+
+struct DiscoveryItem: Decodable, Identifiable {
+    let id: String
+    let type: String
+    let title: String
+    let href: String
+    let kicker: String
+    let meta: String?
+}
+
+struct EmptyResponse: Decodable {}
