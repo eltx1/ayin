d3dbc2b19cf1a91986b76eb77fee1664475eac00
eltx1 <info.eltx@gmail.com>
2026-09-26 14:33:11 +0300

    task 81: test consent-aware tvOS DAI selection

--- platforms/tvos/AYINTVTests/TVCatalogCapabilityTests.swift
+++ platforms/tvos/AYINTVTests/TVCatalogCapabilityTests.swift
@@ -2,8 +2,11 @@ import XCTest
 @testable import AYINTV
 
 final class TVCatalogCapabilityTests: XCTestCase {
-    func testCreatorTVPrefersServerSideDAIPlaybackURL() throws {
-        let capability = TVLinearCapabilityResponse(
+    private func capability(
+        daiAvailable: Bool = true,
+        playbackUrl: String? = "https://pubads.g.doubleclick.net/ssai/event/asset/stream.m3u8"
+    ) -> TVLinearCapabilityResponse {
+        TVLinearCapabilityResponse(
             hls: .init(
                 available: true,
                 url: "https://media.ayin.stream/linear/index.m3u8",
@@ -11,36 +14,59 @@ final class TVCatalogCapabilityTests: XCTestCase {
             ),
             monetization: .init(
                 dai: .init(
-                    available: true,
-                    playbackUrl: "https://pubads.g.doubleclick.net/ssai/event/asset/stream.m3u8"
+                    available: daiAvailable,
+                    playbackUrl: playbackUrl
                 )
             )
         )
-
-        XCTAssertEqual(
-            capability.preferredPlaybackURL?.absoluteString,
-            "https://pubads.g.doubleclick.net/ssai/event/asset/stream.m3u8"
-        )
-        XCTAssertTrue(capability.usesServerSideDAI)
     }
 
-    func testCreatorTVFallsBackToGenericHLSWhenDAIUnavailable() {
-        let capability = TVLinearCapabilityResponse(
-            hls: .init(
-                available: true,
-                url: "https://media.ayin.stream/linear/index.m3u8",
-                masterUrl: "https://media.ayin.stream/linear/master.m3u8"
-            ),
-            monetization: .init(
-                dai: .init(available: false, playbackUrl: nil)
-            )
+    func testPersonalizedConsentMayUseServerSideDAI() throws {
+        let selection = try XCTUnwrap(
+            capability().playbackSelection(consentMode: .personalized)
         )
-
         XCTAssertEqual(
-            capability.preferredPlaybackURL?.absoluteString,
+            selection.url.absoluteString,
+            "https://pubads.g.doubleclick.net/ssai/event/asset/stream.m3u8"
+        )
+        XCTAssertTrue(selection.usesServerSideDAI)
+    }
+
+    func testNonPersonalizedConsentAddsNpaToDAI() throws {
+        let selection = try XCTUnwrap(
+            capability(
+                playbackUrl: "https://pubads.g.doubleclick.net/ssai/event/asset/stream.m3u8?foo=bar"
+            ).playbackSelection(consentMode: .nonPersonalized)
+        )
+        let components = try XCTUnwrap(
+            URLComponents(url: selection.url, resolvingAgainstBaseURL: false)
+        )
+        XCTAssertEqual(components.queryItems?.first(where: { $0.name == "foo" })?.value, "bar")
+        XCTAssertEqual(components.queryItems?.first(where: { $0.name == "npa" })?.value, "1")
+        XCTAssertTrue(selection.usesServerSideDAI)
+    }
+
+    func testLimitedAdsConsentUsesGenericHLSInsteadOfDAI() throws {
+        let selection = try XCTUnwrap(
+            capability().playbackSelection(consentMode: .limitedAds)
+        )
+        XCTAssertEqual(
+            selection.url.absoluteString,
             "https://media.ayin.stream/linear/master.m3u8"
         )
-        XCTAssertFalse(capability.usesServerSideDAI)
+        XCTAssertFalse(selection.usesServerSideDAI)
+    }
+
+    func testCreatorTVFallsBackToGenericHLSWhenDAIUnavailable() throws {
+        let selection = try XCTUnwrap(
+            capability(daiAvailable: false, playbackUrl: nil)
+                .playbackSelection(consentMode: .personalized)
+        )
+        XCTAssertEqual(
+            selection.url.absoluteString,
+            "https://media.ayin.stream/linear/master.m3u8"
+        )
+        XCTAssertFalse(selection.usesServerSideDAI)
     }
 
     func testSeriesDetailAcceptsMissingReleaseYear() throws {
